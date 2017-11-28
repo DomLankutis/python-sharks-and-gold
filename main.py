7b2f8ab2c38f49de1a6ab6fcbce81730de57f9fe
Domantas <domantas.lankutis@ada.ac.uk>
2017-11-28 16:59:28 +0000

    added sound to the win and loose screen

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import random, time, os, keyboard
+from termcolor import colored
 clear = lambda: os.system('clear')
 
 ROW = 51
 COL = 51
-
 # Gold pos number of gold and its (X, Y) Position
 goldPos = []
 for i in range(random.randint(5,20)):
     temp = []
     temp.append(random.randint(1,ROW-1))
-    temp.append(random.randint(1,COL-1))
+    temp.append(random.randint(1,COL-2))
     goldPos.append(temp)
 
 sharkPos = []
 for i in range(random.randint(1,8)):
     temp = []
     temp.append(random.randint(1,ROW-1))
-    temp.append(random.randint(1,COL-1))
+    temp.append(random.randint(1,COL-2))
     sharkPos.append(temp)
 
 prevChoice = 0
@@ -29,9 +29,9 @@ class Grid():
             l = []
             for x in range(ROW):
                 if x == ROW - 1 or x == 0:
-                    l.append(str('.'))
+                    l.append(colored('.', 'white', 'on_green'))
                 elif i == 0 or i == COL - 1:
-                    l.append('.')
+                    l.append(colored('.', 'white', 'on_green'))
                 else:
                     l.append(' ')
             arr.append(l)
@@ -41,16 +41,12 @@ class Grid():
         global prevChoice
         clear()
         for gold in range(len(goldPos)):
-            arr[goldPos[gold][1]][goldPos[gold][0]] = 'G'
+            arr[goldPos[gold][1]][goldPos[gold][0]] = colored('G', 'white', 'on_yellow')
+        # Shark movement
         for pos in range(len(sharkPos)):
 
             choice = random.randint(0, 4)
-
-            if choice == prevChoice:
-                choice = random.randint(0, 4)
-                prevChoice = choice
-            else:
-                prevChoice = choice
+            prevChoice = choice
             if choice == 0:
                 if 1 < sharkPos[pos][0]: sharkPos[pos][0] -= 1
             if choice == 1:
@@ -60,9 +56,9 @@ class Grid():
             if choice == 3:
                 if len(arr[0]) - 2 > sharkPos[pos][1]: sharkPos[pos][1] += 1
 
-            arr[sharkPos[pos][1]][sharkPos[pos][0]] = 'S'
+            arr[sharkPos[pos][1]][sharkPos[pos][0]] = colored('S', 'white', 'on_blue');
 
-        arr[y][x] = '*'
+        arr[y][x] = colored('*', 'white', 'on_red')
         for i in arr:
             for j in i:
                 print(j, end=" ")
@@ -97,17 +93,18 @@ def main():
                     if X == int(goldPos[i][0]) and Y == int(goldPos[i][1]):
                         goldPos.pop(i)
                         goldPoints += 1
-                        print('True')
 
                 for i in range(len(sharkPos)):
                     if X == int(sharkPos[i][0]) and Y == int(sharkPos[i][1]):
                         clear()
                         print('\n\n\t\t\tGAME OVER')
+                        os.system('export BEEP=/usr/share/sounds/KDE-Sys-App-Error-Critical.ogg && paplay $BEEP')
                         time.sleep(10); exit()
 
                 if len(goldPos) == 0:
                     clear()
                     print('\n\n\t\t\tYOU WIN')
+                    os.system('export BEEP=/usr/share/sounds/Oxygen-Sys-Warning.ogg && paplay $BEEP')
                     time.sleep(10); exit()
                 break
             except:
